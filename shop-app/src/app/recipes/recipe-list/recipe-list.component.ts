569713fe18a82251a81aa034dcb5196e6fd6c0a6
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-05-27 17:29:15 +0300

    Initial commit

---
+++ shop-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('First Recipe', 'First Description', 'https://live.staticflickr.com/1441/25154602981_ab2f17e9f8_b.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
