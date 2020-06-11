ccd39cce4e3d05ea00e8d310de39e0132908f9bd
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-06-11 16:57:47 +0300

    Exercise update

--- routing-exercise/src/app/app.module.ts
+++ routing-exercise/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'users', component: UsersComponent },
   { path: 'servers', component: ServersComponent }
 ];
