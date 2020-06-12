4b09c6fbd755258d92fe5d31cacbd4cbc3fc6938
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-06-12 17:30:24 +0300

    Routing exercise update II

--- routing-exercise/src/app/users/user/user.component.ts
+++ routing-exercise/src/app/users/user/user.component.ts
@@ -15,7 +15,8 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
+
     this.route.params
       .subscribe(
         (params: Params) => {
